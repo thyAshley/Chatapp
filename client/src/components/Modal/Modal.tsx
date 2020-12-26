12c4a14b7b2b9c020f31176f099f132cde6b492f
Ashley <thy.ashley@gmail.com>
2020-12-27 00:49:51 +0800

    added modal

---
+++ client/src/components/Modal/Modal.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import './Modal.scss';
+
+const Modal: React.FC<{ children: any; click: () => void }> = ({
+  children,
+  click,
+}) => {
+  const findByKey = (name: string) => {
+    return children.filter((child: { key: string }) => child.key === name);
+  };
+  const closeModal = (
+    e: React.MouseEvent<HTMLDivElement | HTMLButtonElement>,
+  ) => {
+    e.stopPropagation();
+    if ((e.target as HTMLInputElement).classList.contains('modal-close')) {
+      click();
+    }
+  };
+  return (
+    <div className="modal-mask ">
+      <div className="modal-wrapper modal-close" onClick={closeModal}>
+        <div className="modal-container">
+          <div className="modal-header">{findByKey('header')}</div>
+          <div className="modal-body">{findByKey('body')}</div>
+          <div className="modal-footer">
+            <button className="modal-close" onClick={closeModal}>
+              CLOSE
+            </button>
+            {findByKey('footer')}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { useSelector } from 'react-redux';
 import defaultPortrait from '../../assets/images/default_portrait.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -8,14 +8,21 @@ import './Navbar.scss';
 
 import { RootState } from '../../redux/rootReducer';
 import { logout } from '../../redux/auth/authActions';
+import Modal from '../Modal';
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const [showProfileOptions, setShowProfileOptions] = useState(false);
+  const [showProfileModal, setShowProfileModal] = useState(false);
   const user = useSelector((state: RootState) => state.auth.user);
 
   const onLogoutHandler = () => {
     dispatch(logout());
+  };
+
+  const updateProfileHandler = () => {
+    setShowProfileOptions(false);
+    setShowProfileModal(true);
   };
 
   return (
@@ -31,9 +38,16 @@ const Navbar = () => {
         />
         {showProfileOptions && (
           <div id="profile-options">
-            <p>Update profile</p>
+            <p onClick={updateProfileHandler}>Update profile</p>
             <p onClick={onLogoutHandler}>Logout</p>
           </div>
+        )}
+        {showProfileModal && (
+          <Modal click={() => setShowProfileModal(false)}>
+            <Fragment key="header">Modal Header</Fragment>
+            <Fragment key="body">Modal Body</Fragment>
+            <Fragment key="footer">Modal Footer</Fragment>
+          </Modal>
         )}
       </div>
     </div>
