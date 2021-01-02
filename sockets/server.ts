3deec6fbc5c9954f3d86555eda17925b4051b1f2
Ashley <thy.ashley@gmail.com>
2021-01-02 11:43:52 +0800

    setup socket io

--- sockets/server.ts
+++ sockets/server.ts
@@ -1,13 +1,23 @@
 import path from "path";
+import http from "http";
 import express from "express";
 
 const publicPath = path.join(__dirname, "public");
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server);
 
 app.use(express.static(publicPath));
-console.log(publicPath);
 
-app.listen(PORT, () => {
+io.on("connection", (socket: any) => {
+  console.log("A new user is connected");
+
+  socket.on("disconnect", () => {
+    console.log("User was disconnected from server");
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
